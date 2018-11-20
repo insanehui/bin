f147d18b753f598d48ddc5de1426fe6d07572725
insanehui <269200808@qq.com>
2018-11-20 18:12:55 +0800

    增加了参数，更新了yargs

--- js/numberList.js
+++ js/numberList.js
@@ -2,10 +2,13 @@
  * 给列表编号的一个程序
  */
 const readline = require('readline')
+const argv = require('yargs').argv
+
 import _ from 'lodash'
+
+const indent = argv._[0] || '' // 表示缩进
 
 const rl = readline.createInterface({ input: process.stdin })
 rl.on('line', line => {
-  console.log(`> ${line}`)
+  console.log(`${indent} ${line}`)
 })
-
