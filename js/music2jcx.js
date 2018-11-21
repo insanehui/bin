fb4e83733815d4a839375f15d0bf52ab1f06f4f5
insanehui <269200808@qq.com>
2018-11-22 01:01:09 +0800

    精简一些代码

--- js/music2jcx.js
+++ js/music2jcx.js
@@ -15,44 +15,6 @@ const noteTable = {
   '7' : 'b',
 }
 
-let i = 0
-const score = '1 ( (23) (4 (56)) )'
-// const score = '1 (12) 3 (34)'
-
-function score2tree() {
-  let notes = []
-  while(1) {
-    const c = score[i] // 当前字符
-
-    // 越界直接返回
-    if ( c === undefined ) {
-      return notes
-    } 
-
-    if ( /\s/.test(c) ) {
-      i++
-    } 
-    else if ( c==='(' ) {
-      i++
-      notes.push(score2tree())
-    } 
-    else if ( c===')' ) {
-      i++
-      return notes
-    } 
-    else if ( /\d/.test(c) ) {
-      i++
-      notes.push(c)
-    } 
-    else {
-      i++
-    }
-  }
-}
-
-const tree = score2tree()
-console.log('tree', tree)
-
 function tree2jianpu(tree, cx=4) {
   let output = ''
   const x = cx / tree.length
@@ -72,11 +34,6 @@ function tree2jianpu(tree, cx=4) {
   }
   return output
 }
-
-const jianpu = tree2jianpu(tree)
-console.log('jianpu', jianpu)
-
-const bar_score = '1 ( (23) (4 (56)) ) | 1 (12) 3 (34)'
 
 function parse(score) {
   let res = ''
@@ -119,6 +76,7 @@ function parse(score) {
   return res
 }
 
+const bar_score = '1 ( (23) (4 (56)) ) | 1 (12) 3 (34)'
 const jcx = parse(bar_score)
 console.log('jcx', jcx)
 
