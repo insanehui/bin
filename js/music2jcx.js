9bc9bd7ba2d747b65f49dc099c295d6f924958e6
insanehui <269200808@qq.com>
2018-12-17 14:53:12 +0800

    模块化

--- js/music2jcx.js
+++ js/music2jcx.js
@@ -1,75 +1,7 @@
-const noteTable = {
-  '1' : 'c',
-  '2' : 'd',
-  '3' : 'e',
-  '4' : 'f',
-  '5' : 'g',
-  '6' : 'a',
-  '7' : 'b',
-}
-
-function tree2jianpu(tree, cx=4) {
-  let output = ''
-  const x = cx / tree.length
-  for (const item of tree) {
-    if ( typeof item === 'string' ) {
-      output += noteTable[item]
-      if ( x<1 ) {
-        output += `/${1/x}`
-      } 
-      else if(x>1) {
-        output += x
-      }
-    } 
-    else if ( Array.isArray(item) ) {
-      output += tree2jianpu(item, x)
-    } 
-  }
-  return output
-}
-
-function parse(score) {
-  let res = ''
-  const bars = score.split('|')
-  for (const bar of bars) {
-    let i = 0
-    function bar2tree() {
-      let notes = []
-      while(1) {
-        const c = bar[i] // 当前字符
-
-        // 越界直接返回
-        if ( c === undefined ) {
-          return notes
-        } 
-
-        if ( /\s/.test(c) ) {
-          i++
-        } 
-        else if ( c==='(' ) {
-          i++
-          notes.push(bar2tree())
-        } 
-        else if ( c===')' ) {
-          i++
-          return notes
-        } 
-        else if ( /\d/.test(c) ) {
-          i++
-          notes.push(c)
-        } 
-        else {
-          i++
-        }
-      }
-    }
-    const tree = bar2tree()
-    res += tree2jianpu(tree) + '|'
-  }
-  return res
-}
+import {parse} from './music2jcx/parse.js'
 
 if ( global.window ) {
+  console.log('haha')
   const bar_score = '1 ( (23) (4 (56)) ) | 1 (12) 3 (34)'
   const jcx = parse(bar_score)
   console.log('jcx', jcx)
---
+++ js/music2jcx/parse.js
@@ -0,0 +1,71 @@
+const noteTable = {
+  '1' : 'c',
+  '2' : 'd',
+  '3' : 'e',
+  '4' : 'f',
+  '5' : 'g',
+  '6' : 'a',
+  '7' : 'b',
+}
+
+function tree2jianpu(tree, cx=4) {
+  let output = ''
+  const x = cx / tree.length
+  for (const item of tree) {
+    if ( typeof item === 'string' ) {
+      output += noteTable[item]
+      if ( x<1 ) {
+        output += `/${1/x}`
+      } 
+      else if(x>1) {
+        output += x
+      }
+    } 
+    else if ( Array.isArray(item) ) {
+      output += tree2jianpu(item, x)
+    } 
+  }
+  return output
+}
+
+export function parse(score) {
+  let res = ''
+  const bars = score.split('|')
+  for (const bar of bars) {
+    let i = 0
+    function bar2tree() {
+      let notes = []
+      while(1) {
+        const c = bar[i] // 当前字符
+
+        // 越界直接返回
+        if ( c === undefined ) {
+          return notes
+        } 
+
+        if ( /\s/.test(c) ) {
+          i++
+        } 
+        else if ( c==='(' ) {
+          i++
+          notes.push(bar2tree())
+        } 
+        else if ( c===')' ) {
+          i++
+          return notes
+        } 
+        else if ( /\d/.test(c) ) {
+          i++
+          notes.push(c)
+        } 
+        else {
+          i++
+        }
+      }
+    }
+    const tree = bar2tree()
+    res += tree2jianpu(tree) + '|'
+  }
+  return res
+}
+
