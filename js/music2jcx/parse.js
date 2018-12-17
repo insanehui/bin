965a1a27db7a8c4b0fd5882c8a5964046f517a96
insanehui <269200808@qq.com>
2018-12-17 23:28:08 +0800

    新增一个seq2struct函数，还没试验

--- js/music2jcx/parse.js
+++ js/music2jcx/parse.js
@@ -29,6 +29,33 @@ function tree2jianpu(tree, cx=4) {
   return output
 }
 
+// 将一个音符系列转成结构化的数据（这里的struct也就是之前说的tree）
+function seq2struct(seq) {
+  /*
+   * seq是数组
+   * 递归。遇到序列尾或者 ) 即结束
+   */
+  let notes = []
+  while(1) {
+    const c = seq.shift() // 当前字符
+
+    // 越界直接返回
+    if ( c === undefined || c === ')' ) {
+      return notes
+    } 
+    if ( /\s/.test(c) ) {
+      continue
+    } 
+
+    if ( c==='(' ) {
+      notes.push(seq2struct(seq))
+    } 
+    else if ( /\d/.test(c) ) { // 找到音符
+      notes.push(c)
+    } 
+  }
+}
+
 export function parse(score) {
   let res = ''
   const bars = score.split('|')
