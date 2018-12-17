44caf20f92f7b7ba647aef1476a982018e29cd1a
insanehui <269200808@qq.com>
2018-12-18 00:10:25 +0800

    改写了一下单元测试

--- js/music2jcx/parse.test.js
+++ js/music2jcx/parse.test.js
@@ -1,10 +1,23 @@
 import {parse} from './parse.js'
 
-it('parse', () => {
-  const para = [
-  "1 ( (23) (4 (56)) ) | 1 (12) 3 (34)"
-]
-  const hope = `c2d/2e/2f/2g/4a/4|cc/2d/2ee/2f/2|`
-  const fact = parse(...para)
-  expect(fact).toEqual(hope)
-})
+describe('parse', () => {
+  it('normal', () => {
+    const para = [
+      "1 ( (23) (4 (56)) ) | 1 (12) 3 (34)"
+    ]
+    const hope = `c2d/2e/2f/2g/4a/4|cc/2d/2ee/2f/2|`
+    const fact = parse(...para)
+    expect(fact).toEqual(hope)
+  })
+
+  xit('high and low', () => {
+    const para = [
+      ""
+    ]
+    const hope = `c2d/2e/2f/2g/4a/4|cc/2d/2ee/2f/2|`
+    const fact = parse(...para)
+    expect(fact).toEqual(hope)
+  })
+
+});
+
