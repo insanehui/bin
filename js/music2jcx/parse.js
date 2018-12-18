98d02a128e48f88167f5cf74ec05cc39637e52c4
insanehui <269200808@qq.com>
2018-12-19 00:36:18 +0800

    连音线逻辑的雏形快出来了，但跨小节的还没弄好

--- js/music2jcx/parse.js
+++ js/music2jcx/parse.js
@@ -66,6 +66,28 @@ function text2struct(text) {
 
 // global.lastNote = 'c'
 
+// 将树状的数据结构平铺成序列的格式
+function structFlattern(tree, len = tree.length) {
+  /*
+   * 最终得到一个音符 + 时值组成的数组
+   */
+  let res = []
+  const duration = len / tree.length
+  for (const item of tree) {
+    if ( typeof item === 'string' ) { // 这是一个音符
+      res.push({
+        note : item,
+        duration,
+      })
+    } 
+    // 如果是一串音符
+    else if ( Array.isArray(item) ) {
+      res = [...res, ...structFlattern(item, duration)]
+    } 
+  }
+  return res
+}
+
 // 将有连音线的序列合并
 function seqCollapse(seq) {
   const res = []
@@ -86,37 +108,34 @@ function seqCollapse(seq) {
   return res
 }
 
-// 将树状的数据结构平铺成序列的格式
-function structFlattern(tree, len = tree.length) {
+// 将collapse后的seq转成jcx的简谱格式（一小节）
+function seq2JcxScore(seq) {
   /*
-   * 最终得到一个音符 + 时值组成的数组
+   * 会读取或者修改global.lastNote。因为要处理跨小节的连音线
    */
-  let res = []
-  const duration = len / tree.length
-  for (const item of tree) {
-    if ( typeof item === 'string' ) { // 这是一个音符
-      res.push({
-        note : jcxNote(item),
-        duration,
-      })
+  let output = ''
+  for (const {note, duration} of seq) {
+    output += jcxNote(note)
+    if ( duration<1 ) {
+      output += `/${1/duration}`
     } 
-    // 如果是一串音符
-    else if ( Array.isArray(item) ) {
-      res = [...res, ...structFlattern(item, duration)]
-    } 
+    else if(duration>1) {
+      output += duration
+    }
   }
-  return res
+  return output
 }
 
 function parse2(score) {
   let res = text2struct([...score])
   res = structFlattern(res)
   res = seqCollapse(res)
+  res = seq2JcxScore(res)
   console.log('res', res)
   return res
 }
 
-const a = parse2('- - 6. - (-5.) 5.')
+const a = parse2('6 - (5.5.) 1')
 
 export function parse(score) {
   let res = ''
