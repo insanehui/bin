10fb4be97baaa64fe19ff0148e630c258111ce4d
insanehui <269200808@qq.com>
2018-12-20 01:02:39 +0800

    yaml可以转了

--- js/music2jcx/musicFile/main.js
+++ js/music2jcx/musicFile/main.js
@@ -1,9 +1,15 @@
 /*
  * music文件的整体解析
  */
+import yaml from 'js-yaml'
 
 export default function parse(file) {
   // 先分成两部分，按 ==========（三个以上） 区分
   const parts = file.split(/===+\n/)
-  return parts
+  const header = yaml.load(parts[0])
+
+  return {
+    header,
+    score : parts[1],
+  }
 }
