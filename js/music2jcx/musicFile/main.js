ded0f5c8dfc7b6b5118e7e49b5866986d38e3ccf
insanehui <269200808@qq.com>
2018-12-20 20:30:57 +0800

    貌似把基本的文件信息都收集完毕

--- js/music2jcx/musicFile/main.js
+++ js/music2jcx/musicFile/main.js
@@ -7,12 +7,14 @@ function groupTracks(score) {
   const tracks = {}
   // 按行拆分
   const lines = score.split('\n')
+  let lastTrack = null // 保留上一个音轨. 用于收集歌词. 这里沿用了jcx的形式
   for (const line of lines) {
     // 如果是曲谱音轨
     const trackPre = /^<(\S+)>/
-    const regExec = trackPre.exec(line)
-    if ( regExec ) { // 是个音轨
-      const name = regExec[1]
+    const trackExec = trackPre.exec(line)
+    const lyricsPre = /^:w:/
+    if ( trackExec ) { // 是个音轨
+      const name = trackExec[1]
       if ( !tracks[name] ) {
         tracks[name] = {
           name,
@@ -21,6 +23,10 @@ function groupTracks(score) {
       } 
       const score = line.replace(trackPre, '')
       tracks[name].lines.push(score)
+      lastTrack = name
+    } 
+    else if ( lyricsPre.test(line) ) {
+      tracks[lastTrack].lines.push(line.slice(1)) // 去掉前面的“:”
     } 
   }
   return tracks
