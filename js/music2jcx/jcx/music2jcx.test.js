a625e4e54bf009249bc2cb5ccd6e95af7e7bb7d4
insanehui <269200808@qq.com>
2018-12-21 13:35:22 +0800

    多年以前也加到用例

--- js/music2jcx/jcx/music2jcx.test.js
+++ js/music2jcx/jcx/music2jcx.test.js
@@ -43,6 +43,54 @@ EEFG|GFED|CCDE|D3/2C/2C2|
     expect(fact.trim()).toEqual(hope.trim())
   })
 
+  it('多年以前', () => {
+    const para = [
+`
+title : 多年以前
+artist : 童话吉他编配
+timeSign : 4/4
+tempo : 1/4=90
+key : C
+tracks : 
+  - name: guitar
+    jcx: 
+      - type: tab
+        translate: -12
+      - jianpu
+=================
+<guitar> 1 (12) 3 (34) | 5 (65) 3 - | 5 (43) 2 - | 4 (32) 1- 
+<guitar> 1 (12) 3 (34) | 5 (65) 3 - | 5 (43) 2 (32) | 1---
+<guitar> 5 (43) 2 (5.5.) | 4 (32) 1- | 5 (43) 2 (5.5.) | 4 (32) 1- 
+<guitar> 1 (12) 3 (34) | 5 (65) 3 - | 5 (43) 2 (32) | 1---
+`,
+    ]
+    const hope = `
+%MUSE2
+T: 多年以前
+M: 4/4
+L: 1/4
+Q: 1/4=90
+C: 童话吉他编配
+K: C
+
+V:guitar0 style=tab bracket=10
+V:guitar1 style=jianpu 
+
+[V:guitar0]
+e3e3*1/2d0*1/2d2d2*1/2d3*1/2|c0c2*1/2c0*1/2d2*2|c0d3*1/2d2*1/2d0*2|d3d2*1/2d0*1/2e3*2|
+e3e3*1/2d0*1/2d2d2*1/2d3*1/2|c0c2*1/2c0*1/2d2*2|c0d3*1/2d2*1/2d0d2*1/2d0*1/2|e3*4|
+c0d3*1/2d2*1/2d0f3*1/2f3*1/2|d3d2*1/2d0*1/2e3*2|c0d3*1/2d2*1/2d0f3*1/2f3*1/2|d3d2*1/2d0*1/2e3*2|
+e3e3*1/2d0*1/2d2d2*1/2d3*1/2|c0c2*1/2c0*1/2d2*2|c0d3*1/2d2*1/2d0d2*1/2d0*1/2|e3*4|
+[V:guitar1]
+CC/2D/2EE/2F/2|GA/2G/2E2|GF/2E/2D2|FE/2D/2C2|
+CC/2D/2EE/2F/2|GA/2G/2E2|GF/2E/2DE/2D/2|C4|
+GF/2E/2DG,/2G,/2|FE/2D/2C2|GF/2E/2DG,/2G,/2|FE/2D/2C2|
+CC/2D/2EE/2F/2|GA/2G/2E2|GF/2E/2DE/2D/2|C4|
+    `
+    const fact = convert(...para)
+    expect(fact.trim()).toEqual(hope.trim())
+  })
+
   xit('简单分解', () => {
     const para = [
 `
