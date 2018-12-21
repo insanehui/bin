27e64d0d16a3225fd34376170b8c0fd15d50e984
insanehui <269200808@qq.com>
2018-12-21 20:12:33 +0800

    晕死。为什么谱子不对啊

--- js/music2jcx/WebApp.js
+++ js/music2jcx/WebApp.js
@@ -32,7 +32,50 @@ patterns :
     Am : '"Am"532123'
 =================
 <guitar> %C | %Dm | %Em | %Am | "Am"[54321]$-----
+<melody> 0--| 0-- | 0-- | 0-- | 0-(12) 
+w:                                湖水
+<guitar> %C      | %C       | %Am     | %Am
+<melody> 3 (32) 1| 3 - (26.)| 11 (7.1) | 6.-(6.1)
+w: 是你的眼神 梦想 满天星*辰 心情
+<guitar> %Em      | %Em       | %Am     | %Am
+<melody> 7. (7.6.) 5. | 7. - (7.5.) | 6. (6.1)(7.1) | 6. - (12)
+w: 是一个传说 亘古不变地等候 成长
+
 `)
+/*
+是一扇树叶的门
+童年有一群亲爱的人
+春天是一段路程
+沧海桑田的拥有
+那些我爱的人
+那些离逝的风
+那些永远的誓言一遍一遍
+那些爱我的人
+那些沉淀的泪
+那些永远的誓言一遍一遍
+湖水是你的眼神
+梦想满天星辰
+心情是一个传说
+亘古不变地等候
+成长是一扇树叶的门
+童年有一群亲爱的人
+春天是一段路程
+沧海桑田的拥有
+那些我爱的人
+那些离逝的风
+那些永远的誓言一遍一遍
+那些爱我的人
+那些沉淀的泪
+那些永远的誓言一遍一遍
+我们都曾有过一张天真而忧伤的脸
+手握阳光我们望着遥远
+轻轻的一天天一年又一年
+长大间我们是否还会再唱起心愿
+轻轻的一天天一年又一年
+长大间我们是否还会再唱起心愿
+长大间我们是否还会再唱起心愿
+
+*/
 
 export default class App extends React.PureComponent {
   state = {
@@ -56,6 +99,10 @@ export default class App extends React.PureComponent {
     const res = parseFile(file.value) 
     this.setData(fromMusic(file.value))
     console.log('file', res)
+  }
+
+  componentDidMount(){
+    this.doFile()
   }
 
   render() {
