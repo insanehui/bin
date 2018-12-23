700e66bb57f397ddbb6881acc7b70b0b83a7a333
insanehui <269200808@qq.com>
2018-12-23 14:41:56 +0800

    搞定括号了

--- js/music2jcx/musicFile/parseText.test.js
+++ js/music2jcx/musicFile/parseText.test.js
@@ -1,7 +1,6 @@
 import parse from './parseText.js'
 
 describe('parseText', () => {
-
   it('基础', () => {
     const para = [
       `"C"5[1-]  [2..#1-  ]1 "Am"b2.3'' -#4`
@@ -9,6 +8,23 @@ describe('parseText', () => {
     const hope = [
       { "notes": [ "5" ],  chord : 'C',}, 
       { "notes": [ "1", "-" ] },
+      { "notes": [ "2..", "#1", '-' ] },
+      { "notes": [ "1" ] },
+      { "notes": [ "b2." ], chord : 'Am' },
+      { "notes": [ "3''" ] },
+      { "notes": [ "-" ] },
+      { "notes": [ "#4" ] },
+    ]
+    const fact = parse(...para)
+    expect(fact).toEqual(hope)
+  })
+
+  it('综合', () => {
+    const para = [
+      `"C"(5[1-])  [2..#1-  ]1 "Am"b2.3'' -#4`
+    ]
+    const hope = [
+      [{ "notes": [ "5" ],  chord : 'C',}, { "notes": [ "1", "-" ] },],
       { "notes": [ "2..", "#1", '-' ] },
       { "notes": [ "1" ] },
       { "notes": [ "b2." ], chord : 'Am' },
