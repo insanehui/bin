8d763351a4d5a93710db5111c07fc8b49a524f39
insanehui <269200808@qq.com>
2019-01-21 20:15:24 +0800

    能解析简单的音符

--- js/music2jcx/musicParser/bar.js
+++ js/music2jcx/musicParser/bar.js
@@ -1,14 +1,32 @@
 /*
  * 处理music格式里的一个小节
  */
-// import _ from 'lodash'
+import _ from 'lodash'
 // import {fraction} from 'mathjs'
 // import parseText from './parseText.js'
 
+// 读一个音符
+function readNote(str) {
+  const [a, b] = /^\s*(-|[b#]?\d[.']?)/.exec(str)
+  return [b, str.slice(_.size(a))]
+}
+
 // 收集每个音，"-"也算
 function collectBeats(str) {
+  const beats = []
+  while(1){
+    str = _.trim(str)
+    if ( !str || str === ')' ) {
+      return beats
+    } 
+
+    let [note, res] = readNote(str)
+    str = res
+    beats.push(note)
+  }
 }
 
 export default function parse(str, opt = {}) {
-  return str
+  let beats = collectBeats(str)
+  return beats
 }
