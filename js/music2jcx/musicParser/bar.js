7ed36d2d43695fcac3a7c25b67ed2b7dc5fecf72
insanehui <269200808@qq.com>
2019-01-21 19:47:30 +0800

    清理了大部分代码准备重写

--- js/music2jcx/musicParser/bar.js
+++ js/music2jcx/musicParser/bar.js
@@ -1,80 +1,14 @@
 /*
- * 解释music类型文件. 转成一个序列的格式
- * 目前暂时先处理一个小节. 后面看看怎么扩展
+ * 处理music格式里的一个小节
  */
-import _ from 'lodash'
-import {fraction} from 'mathjs'
-import parseText from './parseText.js'
+// import _ from 'lodash'
+// import {fraction} from 'mathjs'
+// import parseText from './parseText.js'
 
-function expandPatterns(text, patterns) {
-  _.each(patterns, (v, k) => {
-    text = text.replace(RegExp('%'+k+'\\b', 'g'), v)
-  })
-  return text
-}
-
-// 将树状的数据结构平铺成序列的格式
-function structFlattern(tree, len = fraction(tree.length)) {
-  /*
-   * 最终得到一个音符 + 时值组成的数组. 
-   * 注意：这里默认写死1/4为一拍！！，后面再想扩展
-   */
-  let res = []
-  const duration = len.div(tree.length)
-  for (const item of tree) {
-    // 如果是一串音符
-    if ( Array.isArray(item) ) {
-      res = [...res, ...structFlattern(item, duration)]
-    } 
-    else { // 一个音符
-      res.push({
-        note:item,
-        duration,
-      })
-    }
-  }
-  return res
-}
-
-// 将有连音线的序列合并
-function seqCollapse(seq) {
-  const res = []
-  for(const i in seq) {
-    const item = seq[i]
-    const {note, duration} = item
-    // eslint-disable-next-line
-    if ( note != '-' || i === '0') { // 注：这里要用 '0' 而不是 0！否则条件会不成立
-      /*
-       * 如果第一个就是延音线，还是先插入
-       */
-      res.push(item)
-    } 
-    else {
-      const lastNote = res[res.length-1]
-      lastNote.duration = lastNote.duration.add(duration)
-    }
-  }
-  return res
+// 收集每个音，"-"也算
+function collectBeats(str) {
 }
 
 export default function parse(str, opt = {}) {
-  const {beat, patterns} = opt
-  str = expandPatterns(str, patterns)
-
-  // let res = text2struct(str, opt)
-  let res = parseText(str)
-
-  if ( !res.length ) {
-    return []
-  } 
-
-  let dura
-  if ( beat ) {
-    dura = fraction(beat).div('1/4').mul(res.length)
-  } 
-
-  res = structFlattern(res, dura)
-
-  res = seqCollapse(res)
-  return res
+  return str
 }
